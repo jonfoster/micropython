6abadbc4048ba6657562a78f4c06e37a10d7b10d
Jon Foster <jon@jon-foster.co.uk>
2024-04-01 20:12:22 +0100

    shared/timeutils/timeutils.h: Add TIMEUTILS_SECONDS_FROM_1970_TO_EPOCH.
    It's very difficult to write C code that deals with time without knowing
    what the epoch is.  Some of the existing MicroPython code has just been
    copy-pasted twice for the two epochs that are currently supported.
    
    This adds a new define, TIMEUTILS_SECONDS_FROM_1970_TO_EPOCH, which is
    defined in timeutils.h.  This is 0 if the epoch is 1970, or the relevant
    offset otherwise.  Time code can just add or subtract this value, and it
    will work for any epoch.  Two of the functions in timeutils.h have been
    de-duplicated in this way.
    
    Signed-off-by: Jon Foster <jon@jon-foster.co.uk>

--- shared/timeutils/timeutils.h
+++ shared/timeutils/timeutils.h
@@ -58,6 +58,8 @@ mp_uint_t timeutils_mktime_2000(mp_uint_t year, mp_int_t month, mp_int_t mday,
 // Select the Epoch used by the port.
 #if MICROPY_EPOCH_IS_1970
 
+#define TIMEUTILS_SECONDS_FROM_1970_TO_EPOCH (0ULL)
+
 static inline void timeutils_seconds_since_epoch_to_struct_time(uint64_t t, timeutils_struct_time_t *tm) {
     // TODO this will give incorrect results for dates before 2000/1/1
     timeutils_seconds_since_2000_to_struct_time(t - TIMEUTILS_SECONDS_1970_TO_2000, tm);
@@ -73,33 +75,28 @@ static inline uint64_t timeutils_seconds_since_epoch(mp_uint_t year, mp_uint_t m
     return timeutils_seconds_since_2000(year, month, date, hour, minute, second) + TIMEUTILS_SECONDS_1970_TO_2000;
 }
 
-static inline mp_uint_t timeutils_seconds_since_epoch_from_nanoseconds_since_1970(uint64_t ns) {
-    return ns / 1000000000ULL;
-}
-
-static inline uint64_t timeutils_nanoseconds_since_epoch_to_nanoseconds_since_1970(uint64_t ns) {
-    return ns;
-}
-
 #else // Epoch is 2000
+
+#define TIMEUTILS_SECONDS_FROM_1970_TO_EPOCH TIMEUTILS_SECONDS_1970_TO_2000
 
 #define timeutils_seconds_since_epoch_to_struct_time timeutils_seconds_since_2000_to_struct_time
 #define timeutils_seconds_since_epoch timeutils_seconds_since_2000
 #define timeutils_mktime timeutils_mktime_2000
 
+// TODO: Remove this unused function?
 static inline uint64_t timeutils_seconds_since_epoch_to_nanoseconds_since_1970(mp_uint_t s) {
-    return ((uint64_t)s + TIMEUTILS_SECONDS_1970_TO_2000) * 1000000000ULL;
-}
-
-static inline mp_uint_t timeutils_seconds_since_epoch_from_nanoseconds_since_1970(uint64_t ns) {
-    return ns / 1000000000ULL - TIMEUTILS_SECONDS_1970_TO_2000;
-}
-
-static inline int64_t timeutils_nanoseconds_since_epoch_to_nanoseconds_since_1970(int64_t ns) {
-    return ns + TIMEUTILS_SECONDS_1970_TO_2000 * 1000000000ULL;
+    return ((uint64_t)s + TIMEUTILS_SECONDS_FROM_1970_TO_EPOCH) * 1000000000ULL;
 }
 
 #endif
+
+static inline mp_uint_t timeutils_seconds_since_epoch_from_nanoseconds_since_1970(uint64_t ns) {
+    return ns / 1000000000ULL - TIMEUTILS_SECONDS_FROM_1970_TO_EPOCH;
+}
+
+static inline int64_t timeutils_nanoseconds_since_epoch_to_nanoseconds_since_1970(int64_t ns) {
+    return ns + TIMEUTILS_SECONDS_FROM_1970_TO_EPOCH * 1000000000ULL;
+}
 
 int timeutils_calc_weekday(int y, int m, int d);
 
